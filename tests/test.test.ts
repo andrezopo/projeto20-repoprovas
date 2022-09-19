7d6a6a3f3220422b7be0b89d463d70c3fc192829
André Zopolato <andrezopo@gmail.com>
2022-09-19 00:32:17 -0300

    test: estabelece testes para as rotas GET /tests/disciplines e GET /tests/teachers

--- tests/test.test.ts
+++ tests/test.test.ts
@@ -34,4 +34,102 @@ describe("POST /tests", () => {
     expect(result.status).toBe(201);
     expect(dbResult[0]).toEqual(result.body);
   });
+
+  it("Must return status 401 and do not create any test in database", async () => {
+    const test = await testFactory();
+    const result = await supertest(app)
+      .post("/tests")
+      .send(test)
+      .set("Authorization", "Bearer hsuahsuahsuahsuahsuahsuhaushausha");
+    const dbResult = await prisma.tests.findMany({ where: {} });
+
+    expect(result.status).toBe(401);
+    expect(dbResult.length).toBe(0);
+  });
+
+  it("Must return status 422 for not sending token and do not create any test in database", async () => {
+    const test = await testFactory();
+    const result = await supertest(app).post("/tests").send(test);
+    const dbResult = await prisma.tests.findMany({ where: {} });
+
+    expect(result.status).toBe(422);
+    expect(dbResult.length).toBe(0);
+  });
+
+  it("Must return 422 for sending wrong test schema and do not create any test in database", async () => {
+    const test = {};
+    const result = await supertest(app).post("/tests").send(test);
+    const dbResult = await prisma.tests.findMany({ where: {} });
+
+    expect(result.status).toBe(422);
+    expect(dbResult.length).toBe(0);
+  });
+});
+
+describe("GET /tests/disciplines", () => {
+  it("Must return status 200 and return not empty array", async () => {
+    const test = await testFactory();
+    const user = await userFactory();
+    await supertest(app).post("/sign-up").send(user);
+    delete user.confirmPassword;
+    const data = await supertest(app).post("/").send(user);
+    await supertest(app)
+      .post("/tests")
+      .send(test)
+      .set("Authorization", "Bearer " + data.body.token);
+    const result = await supertest(app)
+      .get("/tests/disciplines")
+      .set("Authorization", "Bearer " + data.body.token);
+
+    expect(result.status).toBe(200);
+    expect(result.body.length).toBeGreaterThan(0);
+  });
+
+  it("Must return 422 for not sending token", async () => {
+    const result = await supertest(app).get("/tests/disciplines");
+
+    expect(result.status).toBe(422);
+  });
+
+  it("Must return 401 for sending wrong token", async () => {
+    const result = await supertest(app)
+      .get("/tests/disciplines")
+      .set("Authorization", "Bearer kosaksoaksokaoskasokas");
+
+    expect(result.status).toBe(401);
+  });
+});
+
+describe("GET /tests/teachers", () => {
+  it("Must return status 200 and return not empty array", async () => {
+    const test = await testFactory();
+    const user = await userFactory();
+    await supertest(app).post("/sign-up").send(user);
+    delete user.confirmPassword;
+    const data = await supertest(app).post("/").send(user);
+    await supertest(app)
+      .post("/tests")
+      .send(test)
+      .set("Authorization", "Bearer " + data.body.token);
+    const result = await supertest(app)
+      .get("/tests/teachers")
+      .set("Authorization", "Bearer " + data.body.token);
+
+    expect(result.status).toBe(200);
+    expect(result.body.length).toBeGreaterThan(0);
+  });
+
+  it("Must return 422 for not sending token", async () => {
+    const result = await supertest(app).get("/tests/teachers");
+
+    expect(result.status).toBe(422);
+  });
+
+  it("Must return 401 for sending wrong token", async () => {
+    const result = await supertest(app)
+      .get("/tests/teachers")
+      .set("Authorization", "Bearer zzzzzzzzzzzzzzzzzzzzzz");
+
+    expect(result.status).toBe(401);
+  });
 });
